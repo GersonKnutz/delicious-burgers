716c67ecc685bd83cfdbf968932d4991780fc278
Gerson Knutz <knutzgerson@gmail.com>
2024-12-06 19:24:03 -0300

    Add a project demonstrating HTML, CSS, and JavaScript features

---
+++ products.js
@@ -0,0 +1,10 @@
+const menuOptions = [
+
+    { name: 'X-Salad', price: 8, vegan: false, src: './img/xsalad.png' },
+    { name: 'X-Bacon', price: 7.50, vegan: false, src: './img/xbacon.png' },
+    { name: 'X-Bacon Egg', price: 7, vegan: false, src: './img/bacon-egg.png' },
+    { name: 'Monster', price: 8.50, vegan: false, src: './img/monster.png' },
+    { name: 'Monster Vegan', price: 9, vegan: true, src: './img/monster-vegan.png' },
+    { name: 'X-Vegan', price: 8, vegan: true, src: './img/xvegan.png' },
+
+    ]
